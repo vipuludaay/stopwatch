13a34747c42d6f1a845f0b912e404f81d4d14cb1
Vipul Kumar <vipuludaay@gmail.com>
2024-10-25 23:28:07 +0530

    removed ios, windows, android and linux code

--- android/app/src/main/kotlin/com/example/stopwatch/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.stopwatch
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:stopwatch/main.dart';
+import 'package:Stopwatch/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
